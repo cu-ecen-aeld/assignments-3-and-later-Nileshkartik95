e9d30a2790c78634626eee55754ab962bfb0e346
Nileshkartik95 <nias5972@colorado.edu>
2023-01-23 17:24:05 -0700

    Organized the branching statements and comments

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,39 +1,44 @@
 #!/bin/sh
-# 
-# Author: Nileshkartik Ashokkumar
-
-set -e
-set -u
+: '
+  *****************************************************************************/
+​ * Copyright​ ​(C)​ ​2022​ ​by​ ​Nileshkartik Ashokkumar
+​ *
+​ * ​​Redistribution,​ ​modification​ ​or​ ​use​ ​of​ ​this​ ​software​ ​in​ ​source​ ​or​ ​binary
+​ * ​​forms​ ​is​ ​permitted​ ​as​ ​long​ ​as​ ​the​ ​files​ ​maintain​ ​this​ ​copyright.​ ​Users​ ​are
+​ ​* ​permitted​ ​to​ ​modify​ ​this​ ​and​ ​use​ ​it​ ​to​ ​learn​ ​about​ ​the​ ​field​ ​of​ ​embedded
+​ * software.​ Nileshkartik Ashokkumar ​and​ ​the​ ​University​ ​of​ ​Colorado​ ​are​ ​not​ ​liable​ ​for
+​ ​* ​any​ ​misuse​ ​of​ ​this​ ​material.
+​ * 
+ *****************************************************************************/
+​ ​*​ ​@file​ ​finder.sh
+​ ​*​ ​@brief​ ​a shell script that takes 2 arguments from the user, 1 st argument is file path and 2nd argument is a word
+ * The Functionality of this shell script is to count the presence of the 2nd argument i.e.  word in the files mentioned by the user in the 1st argument
+​ ​*
+​ ​*
+​ ​*​ ​@author​ ​Nileshkartik Ashokkumar
+​ ​*​ ​@date​ ​Oct​ ​10​ ​2022
+​ ​*​ ​@version​ ​1.0
+​ ​*
+'
 
 NO_FILES=0
 NO_COUNT=0
-if [ $# -gt 0 ] 
-then
-	if [ $# -lt 3 ]
+FILE_PATH=$1
+FIND_WORD=$2
+
+if [ $# -eq 2 ] 													#check if the no of arguments are 2
+then		
+	if [ -d "$FILE_PATH" ]												#check if the directory exists
 	then
-		if [ $# -lt 2 ]
-		then 
-			echo "only one arg"
-			exit 1
-		else
-			
-			if [ -d "$1" ]
-			then
-				NO_FILES="$(find "$1" -type f | wc -l)"
-				NO_COUNT="$(grep -r "$2" "$1"| wc -l)"
-				#NO_FILES=$(find $1 -type f | wc -l)
-				echo "The number of files are ${NO_FILES} and the number of matching lines are ${NO_COUNT} " 
-			else
-				echo "$1 not found"
-				exit 1
-			fi
-		fi
+		NO_FILES="$(find "$FILE_PATH" -type f | wc -l)"							#count the total no of files in the given path
+		NO_COUNT="$(grep -r "$FIND_WORD" "$FILE_PATH"| wc -l)"						#count the word in all files in the given path
+		echo "The number of files are ${NO_FILES} and the number of matching lines are ${NO_COUNT} " 	#print the ouput
 	else
-		echo "greater than 2 args"
+		echo "$FILE_PATH not found"										#debug msg to print the file not found if the directory is invlaid
 		exit 1
-		fi
-else
-	echo "less arguments"
-	exit 1
 	fi
+else
+	echo "invalid arguments"											#debug msg if user enters less than 2 arguments
+	exit 1
+fi
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,47 +1,49 @@
 #!/bin/sh
-# 
-# Author: Nileshkartik Ashokkumar
+: '
+  *****************************************************************************/
+​ * Copyright​ ​(C)​ ​2022​ ​by​ ​Nileshkartik Ashokkumar
+​ *
+​ * ​​Redistribution,​ ​modification​ ​or​ ​use​ ​of​ ​this​ ​software​ ​in​ ​source​ ​or​ ​binary
+​ * ​​forms​ ​is​ ​permitted​ ​as​ ​long​ ​as​ ​the​ ​files​ ​maintain​ ​this​ ​copyright.​ ​Users​ ​are
+​ ​* ​permitted​ ​to​ ​modify​ ​this​ ​and​ ​use​ ​it​ ​to​ ​learn​ ​about​ ​the​ ​field​ ​of​ ​embedded
+​ * software.​ Nileshkartik Ashokkumar ​and​ ​the​ ​University​ ​of​ ​Colorado​ ​are​ ​not​ ​liable​ ​for
+​ ​* ​any​ ​misuse​ ​of​ ​this​ ​material.
+​ * 
+ *****************************************************************************/
+​ ​*​ ​@file​ ​writer.sh
+​ ​*​ ​@brief​ ​a shell script that takes 2 arguments from the user, 1 st argument is file path and 2nd argument is a word that has to be overwritten in file mentioned in 1 st argument 
+​ ​*
+​ ​*
+​ ​*​ ​@author​ ​Nileshkartik Ashokkumar
+​ ​*​ ​@date​ ​Oct​ ​10​ ​2022
+​ ​*​ ​@version​ ​1.0
+​ ​*
+​ ​'
 
-set -e
-set -u
 
-WRITE_FILE=0
-WRITE_STR=0
-if [ $# -gt 0 ] 
+WRITE_FILE=$1								#store the argument 1 in a variable
+WRITE_STR=$2								#store the argument 2 in a variable
+
+if [ $# -ne 2 ] 
 then
-	if [ $# -lt 3 ]
-	then
-		if [ $# -lt 2 ]
-		then 
-			echo "Argument_1 is missing"
-			exit 1
-		else
-			
-			if [ -d "$(dirname "$1")" ]
-			then
-				if [ -f "$(basename "$1")" ]
-				then 
-					echo "directory exist, file exist" 
-					echo "$2" > "$1"
-				else
-					echo "directory exist, creating file"
-					touch "$(basename "$1")"
-					echo "$2" > "$1"
-				fi
-			else
-				echo "creating directory"
-				mkdir -p "$(dirname "$1")" 
-				cd "$(dirname "$1")"
-				touch "$1" 
-				echo "$2" > "$1"
-			fi
-		fi
-	else
-		echo "greater than 2 Arguments"
-		exit 1
-		fi
-else
-	echo "Argument_0 and Argument_1 is missing"
+	echo "Invalid arguments"					#check if the arguments are not equal to 2, print invalid and exit 1
 	exit 1
+fi
+
+if [ -d "$(dirname "$WRITE_FILE")" ]				#check if the directory is valid, where dirname strips the path except file name
+then
+	if [ -f "$(basename "$WRITE_FILE")" ]			#check if the file is valid, where basename extracts only the last word from the directory 
+	then 
+		echo "$WRITE_STR" > "$WRITE_FILE"		#overwrite the word from 2nd argument to file mentioned in the 1st argument 
+	else
+		touch "$(basename "$WRITE_FILE")"		#create the file if the file doesnt exists
+		echo "$WRITE_STR" > "$WRITE_FILE"		#overwrite the word from 2nd argument to file mentioned in the 1st argument 
 	fi
+else
+	mkdir -p "$(dirname "$WRITE_FILE")" 			#create directory if the directory doesnt exists
+	cd "$(dirname "$WRITE_FILE")"				#change directory to the created directory
+	touch "$WRITE_FILE" 					#create the file mentioned by the user in the 1st argument
+	echo "$WRITE_STR" > "$WRITE_FILE"			#overwrite the word from 2nd argument to file mentioned in the 1st argument 
+fi
+
 
